62975f686de4e185227453333ef7d422881acff6
JosMIgue <jojo_miguel20@hotmail.com>
2021-08-19 12:39:39 -0500

    add message example

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  mensaje: String
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ mensaje: req.query.name ?  `Hello ${req.query.name} this is next.js with react :) on this example app on Tejuino.io org.` : 'You didn\'t send your name on querystring :(' });
 }
